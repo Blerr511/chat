b0b47af6a17aafe7f05806272a54796505b8a186
Blerr511 <lyovghukasyan@gmail.com>
2020-08-30 02:48:01 +0400

    login api login with token and refresh

--- server/api/login.api.js
+++ server/api/login.api.js
@@ -2,6 +2,7 @@ const catchHelper = require("../helpers/catch.helper");
 const User = require("../mongodb/schemas/user.schema");
 const { jwtSecret } = require("../config");
 const { sign } = require("jsonwebtoken");
+const { userErrors } = require("../messages/error/mongoose.error");
 /**
  * Handling login request
  * @param {ExpressRequest} req - request
@@ -12,7 +13,13 @@ const { sign } = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
     try {
         const { username, password } = req.query;
-        const user = await User.authenticate(username, password);
+
+        if (!req.user) {
+            if (!username) throw new Error(userErrors.username_required);
+            if (!password) throw new Error(userErrors.password_required);
+        }
+
+        const user = req.user ?? (await User.authenticate(username, password));
         const token = sign(user, jwtSecret, { expiresIn: "10 h" });
         req.response = {
             code: 200,
