ad7d693dd4ca88ae97e8aff0978ca34faffe920b
Blerr511 <lyovghukasyan@gmail.com>
2020-08-31 00:44:05 +0400

    CLIENT / added user reducer and service

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import auth from "./auth.reducer";
 import rooms from "./room.reducer";
+import users from "./users.reducer";
 
 const reducer = combineReducers({
     auth,
     rooms,
+    users,
 });
 
 export default reducer;
---
+++ client/src/services/users.service.js
@@ -0,0 +1,20 @@
+import { authHeader } from "../helpers/headers";
+import { jsonToQueryParams } from "../helpers/jsonToQueryParams";
+import { handleResponse } from "../helpers/handleResponse";
+
+const search = async ({ search, page = 0, limit = 50 }) => {
+    const initRequest = {
+        headers: { ...authHeader() },
+    };
+    const data = await fetch(
+        jsonToQueryParams(process.env.REACT_APP_USERS, { search, page, limit }),
+        initRequest
+    )
+        .then(handleResponse)
+        .then((data) => data.data);
+    return data;
+};
+
+export const userServices = {
+    search,
+};
