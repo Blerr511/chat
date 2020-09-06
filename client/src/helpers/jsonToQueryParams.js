ce5f4d8c3e0254d15c94fdac8fd04038ca165e12
Blerr511 <lyovghukasyan@gmail.com>
2020-09-06 20:50:05 +0400

    jsonToQueryParams fix

--- client/src/helpers/jsonToQueryParams.js
+++ client/src/helpers/jsonToQueryParams.js
@@ -1,9 +1,18 @@
 export const jsonToQueryParams = (apiUrl, data = {}) => {
-	const url = new URL(apiUrl);
-	for (const k in data) {
-		if (data[k] !== null && data[k] !== undefined)
-			url.searchParams.append(k, data[k]);
-	}
+    try {
+        const url = new URL(apiUrl);
+        for (const k in data) {
+            if (data[k] !== null && data[k] !== undefined)
+                url.searchParams.append(k, data[k]);
+        }
 
-	return url.toString();
+        return url.toString();
+    } catch (error) {
+        const urlParams = new URLSearchParams();
+        for (const k in data) {
+            if (data[k] !== null && data[k] !== undefined)
+                urlParams.append(k, data[k]);
+        }
+        return apiUrl + "?" + urlParams.toString();
+    }
 };
