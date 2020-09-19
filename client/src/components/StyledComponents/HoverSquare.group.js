38a575396436ffc97aad03c7d0651c13da8f7c7d
Blerr511 <lyovghukasyan@gmail.com>
2020-09-19 14:54:46 +0400

    CLIENT / mui styling changes

--- client/src/components/StyledComponents/HoverSquare.group.js
+++ client/src/components/StyledComponents/HoverSquare.group.js
@@ -1,11 +1,7 @@
-import React from "react";
-import {
-    Avatar as AVATAR,
-    IconButton as ICONBUTTON,
-    makeStyles,
-} from "@material-ui/core";
-
-const useAvatarStyles = makeStyles((theme) => ({
+import { withStyles } from "@material-ui/core";
+import _Avatar from "@material-ui/core/Avatar";
+import _IconButton from "@material-ui/core/IconButton";
+const avatarStyles = (theme) => ({
     root: {
         position: "relative",
         width: "50px",
@@ -47,21 +43,53 @@ const useAvatarStyles = makeStyles((theme) => ({
             backgroundColor: theme.palette.primary.light,
         },
     },
-}));
+});
+const iconButtonStyles = (theme) => ({
+    root: {
+        position: "relative",
+        width: "50px",
+        height: "50px",
+        overflow: "visible",
+        "&[data-active=true],&:hover": {
+            borderRadius: "10px",
+        },
+        "&::after": {
+            content: '""',
+            display: "block",
+            width: 5,
+            backgroundColor:
+                theme.palette.type === "dark"
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+            position: "absolute",
+            left: -14,
+            height: 25,
+            top: 13,
+            borderBottomRightRadius: 5,
+            borderTopRightRadius: 5,
+            opacity: 0,
+            transitionDuration: theme.transitions.duration.shorter,
+        },
+        "&:hover::after": {
+            opacity: 1,
+        },
+        "&[data-active='true']::after": {
+            opacity: 1,
+            height: 40,
+            top: 5,
+        },
+        transitionDuration: theme.transitions.duration.shorter,
+    },
+});
 /**
  * @param {import("@material-ui/core").AvatarProps & {'data-active':boolean}} props
  */
-const Avatar = (props) => {
-    const classes = useAvatarStyles();
-    return <AVATAR classes={classes} {...props} />;
-};
+const Avatar = withStyles(avatarStyles)(_Avatar);
+
 /**
  * @param {import("@material-ui/core").IconButtonProps & {'data-active':boolean}} props
  */
-const IconButton = (props) => {
-    const classes = useAvatarStyles();
-    return <ICONBUTTON classes={classes} {...props} />;
-};
+const IconButton = withStyles(iconButtonStyles)(_IconButton);
 
 export const HoverSquare = {
     Avatar,
