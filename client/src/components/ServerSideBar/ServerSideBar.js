4aa54d98d6adc8f7b64c71e218b739b5d3c4dfa7
Blerr511 <lyovghukasyan@gmail.com>
2020-11-09 01:07:55 +0400

    CLIENT / server side bar changes

--- client/src/components/ServerSideBar/ServerSideBar.js
+++ client/src/components/ServerSideBar/ServerSideBar.js
@@ -1,33 +1,37 @@
-import { Button, makeStyles, Paper, Typography } from "@material-ui/core";
-import { Add, ChevronRight } from "@material-ui/icons";
-import React, { useEffect, useState } from "react";
-import usePermissions from "../../hooks/usePermissions.hook";
-import useStorageState from "../../hooks/useStorageState.hook";
-import AddPersonIcon from "../../svg/AddPerson.icon";
-import HashIcon from "../../svg/Hash.icon";
-import SettingsIcon from "../../svg/Settings.icon";
-import XIcon from "../../svg/X.icon";
-import DialogCreateInvite from "../Dialog/DialogCreateInvite";
-import DialogCreateRoom from "../Dialog/DialogCreateRoom";
-import { Styled } from "../StyledComponents/Styled.group";
+import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Add from '@material-ui/icons/Add';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import React, { useEffect, useState } from 'react';
+import usePermissions from '../../hooks/usePermissions.hook';
+import useStorageState from '../../hooks/useStorageState.hook';
+import AddPersonIcon from '../../svg/AddPerson.icon';
+import HashIcon from '../../svg/Hash.icon';
+import SettingsIcon from '../../svg/Settings.icon';
+import XIcon from '../../svg/X.icon';
+import DialogCreateInvite from '../Dialog/DialogCreateInvite';
+import DialogCreateRoom from '../Dialog/DialogCreateRoom';
+import { Styled } from '../StyledComponents/Styled.group';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
         width: 240,
-        overflow: "hidden",
-        display: "flex",
-        flexDirection: "column",
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
         borderRadius: 0,
         backgroundColor: theme.palette.background.secondary,
-        height: "100%",
+        height: '100%',
     },
     header: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
         padding: theme.spacing(0, 2),
-        height: "48px",
-        boxSizing: "border-box",
+        height: '48px',
+        boxSizing: 'border-box',
         backgroundColor: theme.palette.background.secondary,
         boxShadow: theme.shadows[1],
     },
@@ -36,131 +40,131 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.text.primary,
         fontWeight: 600,
         lineHeight: 20,
-        textOverflow: "ellipsis",
+        textOverflow: 'ellipsis',
     },
     inviteBlock: {
-        textAlign: "center",
-        position: "relative",
+        textAlign: 'center',
+        position: 'relative',
         boxShadow: `inset 0 -1px 0 ${theme.palette.background.modifierAccent}`,
-        background: `url(${require("../../assets/img/invite_new_m.svg")}) no-repeat center 20px`,
-        padding: "73px 20px 20px",
+        background: `url(${require('../../assets/img/invite_new_m.svg')}) no-repeat center 20px`,
+        padding: '73px 20px 20px',
     },
     inviteText: {
-        fontSize: "14px",
+        fontSize: '14px',
         color: theme.palette.text.normal,
-        lineHeight: "18px",
+        lineHeight: '18px',
         marginTop: theme.spacing(1),
-        "& span": {
-            display: "block",
+        '& span': {
+            display: 'block',
         },
     },
     roomContainer: {
-        display: "flex",
-        flexDirection: "column",
-        boxSizing: "border-box",
-        flex: "1 1 auto",
-        boxShadow: "none",
+        display: 'flex',
+        flexDirection: 'column',
+        boxSizing: 'border-box',
+        flex: '1 1 auto',
+        boxShadow: 'none',
         paddingRight: theme.spacing(1),
     },
     roomHeaderContainer: {
         paddingLeft: theme.spacing(2),
         paddingRight: theme.spacing(1),
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        position: "relative",
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        position: 'relative',
         height: theme.spacing(3),
     },
     roomHeaderTitle: {
-        justifyContent: "flex-start",
-        width: "100%",
+        justifyContent: 'flex-start',
+        width: '100%',
         fontSize: 12,
         padding: 0,
         margin: 0,
-        textTransform: "uppercase",
-        textOverflow: "ellipsis",
+        textTransform: 'uppercase',
+        textOverflow: 'ellipsis',
         color: theme.palette.text.channelsDefault,
         fontWeight: 600,
-        lineHeight: "16px",
-        flex: "1 1 auto",
-        cursor: "pointer",
-        "&:hover": {
+        lineHeight: '16px',
+        flex: '1 1 auto',
+        cursor: 'pointer',
+        '&:hover': {
             color: theme.palette.text.interactiveHover,
         },
     },
     roomTitle: {
-        display: "flex",
-        justifyContent: "space-between",
+        display: 'flex',
+        justifyContent: 'space-between',
         marginLeft: theme.spacing(1),
         width: `calc(100% - ${theme.spacing(1)}px)`,
         fontSize: theme.typography.pxToRem(12),
         paddingLeft: theme.spacing(1),
         color: theme.palette.text.channelsDefault,
-        marginBottom: "2px",
-        "&[data-active=true]": {
+        marginBottom: '2px',
+        '&[data-active=true]': {
             backgroundColor: theme.palette.background.modifierSelected,
             color: theme.palette.text.interactiveActive,
         },
-        "&[data-active=false]:hover": {
+        '&[data-active=false]:hover': {
             backgroundColor: theme.palette.background.modifierHover,
             color: theme.palette.text.interactiveHover,
         },
     },
     headerContainer: {
-        display: "flex",
-        position: "relative",
+        display: 'flex',
+        position: 'relative',
         paddingTop: theme.spacing(2),
     },
     inviteButton: {
-        fontFamily: "Whitney,Helvetica Neue,Helvetica,Arial,sans-serif",
-        border: "none",
-        borderRadius: "3px",
-        fontSize: "14px",
-        height: "38px",
+        fontFamily: 'Whitney,Helvetica Neue,Helvetica,Arial,sans-serif',
+        border: 'none',
+        borderRadius: '3px',
+        fontSize: '14px',
+        height: '38px',
         padding: theme.spacing(0.25, 2),
-        minWidth: "96px",
-        width: "auto",
+        minWidth: '96px',
+        width: 'auto',
         color: theme.palette.text.primary,
-        margin: theme.spacing(2, "auto", 0),
-        textRendering: "optimizeLegibility",
-        textTransform: "none",
+        margin: theme.spacing(2, 'auto', 0),
+        textRendering: 'optimizeLegibility',
+        textTransform: 'none',
     },
     closeInviteBlock: {
-        position: "absolute",
-        top: "12px",
-        right: "16px",
-        width: "18px",
-        height: "18px",
+        position: 'absolute',
+        top: '12px',
+        right: '16px',
+        width: '18px',
+        height: '18px',
         color: theme.palette.text.interactiveNormal,
-        cursor: "pointer",
-        "&:hover": {
+        cursor: 'pointer',
+        '&:hover': {
             color: theme.palette.text.interactiveHover,
         },
     },
     expandIcon: {
-        position: "absolute",
+        position: 'absolute',
         left: 2,
         top: 6,
         width: theme.shape.iconSmallest,
         height: theme.shape.iconSmallest,
-        transition: "transform .2s ease-out,-webkit-transform .2s ease-out",
-        "&[data-expanded=true]": {
-            transform: "rotate(90deg)",
+        transition: 'transform .2s ease-out,-webkit-transform .2s ease-out',
+        '&[data-expanded=true]': {
+            transform: 'rotate(90deg)',
         },
     },
     newRoomIcon: {
         padding: 0,
         width: 18,
         height: 18,
-        flex: "0 0 auto",
-        "& svg": {
+        flex: '0 0 auto',
+        '& svg': {
             width: 18,
             height: 18,
         },
         color: theme.palette.text.channelsDefault,
-        cursor: "pointer",
-        "&:hover": {
+        cursor: 'pointer',
+        '&:hover': {
             color: theme.palette.text.interactiveHover,
         },
     },
@@ -168,14 +172,14 @@ const useStyles = makeStyles((theme) => ({
         width: theme.shape.iconSmaller,
         height: theme.shape.iconSmaller,
         color: theme.palette.text.muted,
-        marginRight: "6px",
+        marginRight: '6px',
     },
     actionContainer: {
-        display: "flex",
+        display: 'flex',
     },
     actionIcon: {
         color: theme.palette.text.interactiveNormal,
-        "&:hover": {
+        '&:hover': {
             color: theme.palette.text.interactiveHover,
         },
     },
@@ -191,11 +195,11 @@ const ServerSideBar = ({
     createNewRoom,
     setSelectedTextChannel,
 }) => {
-    const name = server.get("name"),
-        rooms = server.get("rooms"),
-        members = server.get("members"),
-        id = server.get("_id"),
-        selectedTextChannel = server.get("activeRoom");
+    const name = server.get('name'),
+        rooms = server.get('rooms'),
+        members = server.get('members'),
+        id = server.get('_id'),
+        selectedTextChannel = server.get('activeRoom');
     const classes = useStyles();
     const [textChannelExpanded, setTextChannelExpanded] = useStorageState(
         `app.server_${id}.textChannelCollapsed`,
@@ -204,7 +208,7 @@ const ServerSideBar = ({
     const [createRoomDialog, setCreateRoomDialog] = useState(false);
     const [createInviteDialog, setCreateInviteDialog] = useState(false);
     const [hideInviteBlock, setHideInviteBlock] = useStorageState(
-        "app.hideInviteBlock",
+        'app.hideInviteBlock',
         false
     );
     const handleTextExpandClick = () => {
@@ -219,11 +223,11 @@ const ServerSideBar = ({
         setHideInviteBlock(true);
     };
     const [, myMember] = members.findEntry(
-        (v) => v.getIn(["user", "_id"]) === myUser?.get("_id")
+        (v) => v.getIn(['user', '_id']) === myUser?.get('_id')
     );
-    const checkPermissions = usePermissions(myMember?.getIn(["role", "name"]));
+    const checkPermissions = usePermissions(myMember?.getIn(['role', 'name']));
     const handleSubmitNewRoom = ({ name, type }) => {
-        if (type === "text") createNewRoom(id, { name });
+        if (type === 'text') createNewRoom(id, { name });
         handleCloseDialog();
     };
 
@@ -265,7 +269,7 @@ const ServerSideBar = ({
                                 className={classes.closeInviteBlock}
                             />
                             <Typography className={classes.inviteText}>
-                                <span>{"An adventure begins."}</span>
+                                <span>{'An adventure begins.'}</span>
                                 <span>{"Let's add some friends!"}</span>
                             </Typography>
                             <Button
@@ -292,7 +296,7 @@ const ServerSideBar = ({
                                 />
                                 Text channels
                             </Typography>
-                            {checkPermissions("createRoom") && (
+                            {checkPermissions('createRoom') && (
                                 <Typography
                                     onClick={handleOpenDialog}
                                     className={classes.newRoomIcon}
@@ -306,7 +310,7 @@ const ServerSideBar = ({
                     {rooms && textChannelExpanded ? (
                         rooms.map((el, i) => {
                             return (
-                                <div key={el.get("_id")}>
+                                <div key={el.get('_id')}>
                                     <Button
                                         variant="text"
                                         className={classes.roomTitle}
@@ -320,23 +324,23 @@ const ServerSideBar = ({
                                     >
                                         <span
                                             style={{
-                                                display: "flex",
-                                                alignItems: "center",
+                                                display: 'flex',
+                                                alignItems: 'center',
                                             }}
                                         >
                                             <HashIcon
                                                 className={classes.iconHash}
-                                            />{" "}
+                                            />{' '}
                                             <Typography
                                                 style={{
-                                                    textTransform: "none",
+                                                    textTransform: 'none',
                                                     lineHeight: 1,
                                                 }}
                                             >
-                                                {el.get("name")}
+                                                {el.get('name')}
                                             </Typography>
                                         </span>
-                                        {checkPermissions("editRoom") &&
+                                        {checkPermissions('editRoom') &&
                                             selectedTextChannel === i && (
                                                 <div
                                                     className={
@@ -344,7 +348,7 @@ const ServerSideBar = ({
                                                     }
                                                 >
                                                     <Styled.ToolTip
-                                                        title={"Create Invite"}
+                                                        title={'Create Invite'}
                                                         placement="top"
                                                         arrow
                                                     >
@@ -355,14 +359,14 @@ const ServerSideBar = ({
                                                         />
                                                     </Styled.ToolTip>
                                                     <Styled.ToolTip
-                                                        title={"Edit channel"}
+                                                        title={'Edit channel'}
                                                         placement="top"
                                                         arrow
                                                     >
                                                         <SettingsIcon
                                                             style={{
                                                                 marginLeft:
-                                                                    "4px",
+                                                                    '4px',
                                                             }}
                                                             className={
                                                                 classes.actionIcon
@@ -376,7 +380,7 @@ const ServerSideBar = ({
                             );
                         })
                     ) : (
-                        <div key={rooms.getIn([selectedTextChannel, "_id"])}>
+                        <div key={rooms.getIn([selectedTextChannel, '_id'])}>
                             <Button
                                 variant="text"
                                 className={classes.roomTitle}
@@ -390,27 +394,27 @@ const ServerSideBar = ({
                             >
                                 <span
                                     style={{
-                                        display: "flex",
-                                        alignItems: "center",
+                                        display: 'flex',
+                                        alignItems: 'center',
                                     }}
                                 >
-                                    <HashIcon className={classes.iconHash} />{" "}
+                                    <HashIcon className={classes.iconHash} />{' '}
                                     <Typography
                                         style={{
-                                            textTransform: "none",
+                                            textTransform: 'none',
                                             lineHeight: 1,
                                         }}
                                     >
                                         {rooms.getIn([
                                             selectedTextChannel,
-                                            "name",
+                                            'name',
                                         ])}
                                     </Typography>
                                 </span>
-                                {checkPermissions("editRoom") && (
+                                {checkPermissions('editRoom') && (
                                     <div className={classes.actionContainer}>
                                         <Styled.ToolTip
-                                            title={"Create Invite"}
+                                            title={'Create Invite'}
                                             placement="top"
                                             arrow
                                         >
@@ -419,13 +423,13 @@ const ServerSideBar = ({
                                             />
                                         </Styled.ToolTip>
                                         <Styled.ToolTip
-                                            title={"Edit channel"}
+                                            title={'Edit channel'}
                                             placement="top"
                                             arrow
                                         >
                                             <SettingsIcon
                                                 style={{
-                                                    marginLeft: "4px",
+                                                    marginLeft: '4px',
                                                 }}
                                                 className={classes.actionIcon}
                                             />
