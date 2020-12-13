ae2078dec36643960cefbfe2f3e4537f92ee089c
Blerr511 <lyovghukasyan@gmail.com>
2020-12-14 02:05:57 +0400

    SERVER / static controller fix

--- server/controller/static.controller.js
+++ server/controller/static.controller.js
@@ -1,10 +1,10 @@
-const path = require("path");
-
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
 /**
  * @type {import('express').RequestHandler}
  */
 const page = (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(appDir, 'build', 'index.html'));
 };
 
 module.exports = { page };
